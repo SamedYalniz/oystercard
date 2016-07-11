66bb4823ffb46842f6e811c000d05e0e9fedf5df
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-11 18:57:56 +0200

    Complete Challenge #05, add top_up method

---
+++ lib/oystercard.rb
@@ -0,0 +1,12 @@
+class Oystercard
+  attr_accessor :balance
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    self.balance += amount
+    self.balance
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
-require "oystercard"
+require 'oystercard'
 describe Oystercard do
-end 
+  subject(:card){described_class.new}
+
+  it 'freshly initialized card has 0 balance' do
+    expect(card.balance). to eq 0
+  end
+
+  it 'tops up the card' do
+    random_number = rand(1000)
+    card.top_up(random_number)
+    expect(card.balance).to eq random_number
+  end
+
+end
